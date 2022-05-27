bef1b5e12b8dad93c8883e1fe0d382f39c9ab4c7
PearAmp <pearamp1999@gmail.com>
2022-05-27 12:02:05 +0700

    first commit

---
+++ components/CreateaccountPage/CreateaccountPage.vue
@@ -0,0 +1,73 @@
+<template>
+<div class="container">
+    <div class="heading">
+        <div class="text-left">
+            <h1>Create an Account</h1>
+<div class="textContent">
+    <p>Lorem ipsum dolor </p>
+<p>sit amet consectetur</p>
+</div>
+        </div>
+<div class="inputItems pt-5">
+    <v-row>
+    <v-col cols="12">
+    <input type="text" name="name" id="name"  placeholder="Name"/>
+
+</v-col>
+ <v-col cols="12" class="pt-10">
+    <input type="text" name="email" id="email"  placeholder="email"/>
+
+</v-col>
+ <v-col cols="12" class="pt-10">
+    <input type="text" name="password" id="password"  placeholder="password"/>
+
+</v-col>
+<v-col cols="12" class="pt-10">
+    <input type="text" name="confirmpassword" id="confirmpassword"  placeholder="confirm password"/>
+
+</v-col>
+</v-row>
+</div>
+
+<div class="submit pt-10">
+     <v-row>
+         <v-col cols="6">
+              <input class="botton" type="submit" value="SIGN UP">
+         </v-col>
+         <v-col cols="6">
+             <p>Lorem ipsum dolor sit amet consectetur</p>
+         </v-col>
+     </v-row>
+</div>
+    </div>
+</div>
+  
+</template>
+<script>
+export default {
+    name: 'CreateaccountPage'
+
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  max-width: 1320px;
+  
+}
+
+input {
+    border: 1px solid #000;
+    height: 50px;
+    width: 80%;
+}
+.botton {
+    background-color: #000;
+    color: #ffff;
+    width: 100%;
+}
+.textContent {
+    padding-top: 50px;
+    line-height: 0.5;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <SigninPage />
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
+<style lang="scss" scoped>
+.container {
+  width: 50%;
+}
+@media screen and (max-width: 1420px) {
+ .container {
+  width: 80%;
+} 
+  
+}
+@media screen and (max-width: 991px) {
+ .container {
+  width: 100%;
+} 
+  
+}
+@media screen and (max-width: 599px) {
+ .container {
+  width: 100%;
+} 
+  
+}
+</style>
