a35b4259f48e0338e1c40a6b1c96c347810f32fa
PearAmp <pearamp1999@gmail.com>
2022-05-30 14:23:40 +0700

    edit

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <SigninPage />
-    
   </div>
 </template>
 
@@ -14,6 +13,15 @@ export default {
 * {
   padding: 0;
   margin: 0;
-
+}
+@media screen and (max-width: 599px) {
+  * {
+    height: 100%;
+  }
+}
+@media screen and (min-width: 768px) and (max-width: 991px) {
+  * {
+    height: 100%;
+  }
 }
 </style>
